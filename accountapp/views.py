8aad725b6c87992fe4a7de14ae54d498977a587c
jusung-c <62228433+jusung-c@users.noreply.github.com>
2021-10-23 22:55:23 +0900

    base.html

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_world(request):
-    return HttpResponse('Hello World!')
+    # HttpRestponse는 response를 직접 만들어서 되돌려주는 형태
+    # return HttpResponse('Hello World!')
+
+    return render(request, 'base.html')
