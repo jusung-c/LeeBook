df9266fbdc6da0dae5dcc31a2fb95673da5cbef6
jusung-c <62228433+jusung-c@users.noreply.github.com>
2021-10-27 23:02:25 +0900

    delete view

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 # accountapp:hello_world (app_name:name)으로 바로 라우팅할 수 있도록
 app_name = "accountapp"
@@ -16,5 +16,5 @@ urlpatterns = [
     path('create/', AccountCreateView.as_view(), name='create'),
     path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
     path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
-
+    path('delete/<int:pk>', AccountDeleteView.as_view(), name='delete'),
 ]
